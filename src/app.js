9b7d2e510fdefda64fdc0bbd0fb2bf6f571cfaab
g8fyol <g8fyol@g8fyols-MacBook-Air.local>
2025-05-05 01:02:47 +0530

    middleware and error handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,120 @@
 const express = require('express');
+// const {adminAuth} = require("./middlewares");
+// const {adminAuth, userAuth} = require("./middlewares/auth")
 
 const app = express();
 
 
+
 // handling the requests 
-app.use("/test", (req, res)=>{
-    res.send("hello from the server!");
+// instead of app.use we will use app.get and app.post other method to handle different calls separetly
+// this will only handel get call to /user
+
+//mathes /user, /user/anything/anything 
+//? is optional
+//+ abc abbbbc atleast 1 b 
+//* ab*c any thing in between b and c basically we can put regex in here to match url
+// a(bc)+d atleast one bc in between 
+// app.get(/a(bc)+d/,(req, res)=>{
+//     res.send("? indicates optional");
+// })
+// app.get("/user/:userId/:name/:password", (req, res)=>{
+//     console.log(req.params);
+//     console.log(req.query); //it will give all the query parameters
+//     res.send({firstName: "g8fyol", lastName: "test"});
+// })
+//handling dynamic apis like user/101 or user/104444 :useId
+// app.use('/user/',(req, res)=>{
+//     res.send("haha order matters"); //other handeler will not get chance as it will handle all
+// })
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: "g8fyol", lastName: "test"});
+// })
+// app.post("/user",(req, res)=>{
+//     console.log("saving data to database");
+//     res.send("data saved successfully");
+// })
+// app.delete('/user', (req, res)=>{
+//     res.send("deleted successfully");
+// })
+
+// app.use("/test", (req, res)=>{
+//     res.send("hello from the server!");
+// })
+
+//app.use will match the all the http request at give route
+// app.use("/hello", (req, res)=>{
+//     res.send("hello hello hello!");
+// })
+// app.use("/hello/2", (req, res)=>{
+    //this function is called route handler
+//     res.send("it will match /hello/2 ");
+// })
+// app.use("/nodemon",(req, res)=>{
+//     res.send("nodemon tested"); //if we don't write res.send() this will just create a infinite req hanging around 
+// })
+
+// app.use("/user",
+//     [(req, res, next)=>{
+//         console.log("handling the route user!!");
+//         //res.send("RESponse1!!"); //as soon as we do res.send it will not go furthur
+//         next(); // if we do next it will go to next parameter handler so if res.send is not done here it will goto next handler
+//         // if we do so if write both then we will try to send other response to same request it will give error
+//         // next basically calls next route handler
+//         // if we call next() in last handler ther it will give route error as we are not handling that route
+//     },
+//     (req, res, next)=>{
+//         console.log("hanlding the route user 2");
+//         res.send("response3");
+//     },
+//     //these all called middle wares and express will go through the chain of middleware
+
+//     // GET /user ==> it goes through middleware chain ==>> those function which actually handle the request is called request handler
+//     // why we need middleware 
+    
+//     (req, res, next)=>{
+//         console.log("hanlding the route user 2");
+//         res.send("response2");
+//     }]
+// )
+
+//writing middleware for admin so that we can do authorization at beginning and do next
+// app.use("/admin", adminAuth);
+// app.use("/user/data", userAuth, (req, res)=>{
+//     res.send("user DATA send");
+// });
+// //if we don't need middleware we can directly call the user
+// app.use("/user/login",(req, res)=>{
+//     res.send("logged in successfully");
+// })
+
+// app.get("/admin/getAllData", (req, res)=>{
+//     //check if user is admin or not 
+//     // const token = "xyz";
+//     // const isAdminAuthorized = token === "xyz";
+//     res.send("send all data");  
+
+// })
+// app.get("/admin/deleteUser", (req, res)=>{
+//     res.send("user is deleted");
+// })
+
+//error handling
+app.get("/getuserdata", (req, res)=>{
+    // logic of db call and get user data 
+    // but if there is some error in the code 
+    throw new Error("error happend"); //error should not be exposed like this
+    //try to write code in try and catch but still some unhandeled error
+
+    res.send("user data sent");
 })
-app.use("/hello", (req, res)=>{
-    res.send("hello hello hello!");
-})
-app.use("/nodemon",(req, res)=>{
-    res.send("nodemon tested");
+
+app.use("/",(err ,req, res, next)=>{
+    //this is wild card
+    if(err){
+        //we can log error in sentry and all
+        res.status(500).send("something went wrong");
+    }
 })
 
 app.listen(3000, ()=> {
---
+++ todo.md
@@ -0,0 +1,14 @@
+-init git
+-git ignore
+-repo creation
+-push code
+
+
+- order of routes matter a lot
+
+- install postman and set it for our project 
+- regex and dynamic routers as routes
+---------------------------------------------------
+-- multiple route handler and next() function
+-- middleware and how express handle request behind the scene
+-- error handling using app.use("/", (err, req, res, next)) to handle it gracefully 
